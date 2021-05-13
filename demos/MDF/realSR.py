485d2024e99fcb10ee26c7d7464fcdcf1044ee3c
Emma J Reid <reid59@purdue.edu>
2021-05-13 15:19:26 -0400

    Added SR Code & Priors

--- demos/MDF/realSR.py
+++ demos/MDF/realSR.py
@@ -65,7 +65,6 @@ def mace(LR,numagents,c,args):
     W = np.copy(X)
     
     # Initialize vectors for metric analysis.
-    PSNR = np.zeros(args.iterstop)
     maceerr = np.zeros(args.iterstop)
 
     # MACE Framework
@@ -76,12 +75,11 @@ def mace(LR,numagents,c,args):
         W = W+2*args.rho*(Z-X)
 
      # Save metrics for this iteration to the vector.       
-        PSNR[i] = psnr.psnr(gt/255,G(W,numagents,args.mu)[:,0].reshape(mdim,ndim))
 
         Y=np.copy(W)
         maceerr[i]=(1/args.sign)*np.linalg.norm(G(Y,numagents,args.mu)-L(Y,Y,numagents, mdim, ndim, c, LR, args))/np.linalg.norm(G(Y,numagents,args.mu))
 
-    return G(W,numagents,args.mu)[:,0], PSNR, maceerr
+    return G(W,numagents,args.mu)[:,0], maceerr
 
 #L takes in all of the state vectors and applies denoisers to the first k state
 #vectors and the forward model to the last state vector.
@@ -180,21 +178,21 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Gather P&P input parameters.")
     parser.add_argument('--SRval', type=int, default=4,help='Super-Resolution factor')
     parser.add_argument('--beta', type=float, default= 0.5, help='Regularization factor')
-    parser.add_argument('--iterstop', type=int, default=1, help='Number of iterations to run')
+    parser.add_argument('--iterstop', type=int, default=20, help='Number of iterations to run')
     parser.add_argument('--sign', type=float,default=0.1, help='Noise level trained to remove')
-    parser.add_argument('--sigy', type=float, default=0, help='Noise level in image')
-    parser.add_argument('--mu', type=float, default=0.5, help='Weighting factor')
+    parser.add_argument('--sigy', type=float, default=0.1, help='Noise level in image')
+    parser.add_argument('--mu', type=float, default=0.2, help='Weighting factor')
     parser.add_argument('--rho', type=float, default=0.5, help='Convergence factor')
     
     parser.add_argument('--model_dir', default=os.path.join('priors'), help='directory of the model')
     
-    #Options for model names are dncnn_25.pth and MDF.pth
-    parser.add_argument('--model_name', default='dncnn_25.pth', type=str, help='the model name')
-    parser.add_argument('--hrname', default='nanotest.png', type=str, help='the HR image name')
+    #Options for model names are dncnn_25.pth, pent.pth, and 
+    parser.add_argument('--model_name', default='srnano.pth', type=str, help='the model name')
+    parser.add_argument('--lrname', default='nanotestLR.png', type=str, help='the HR image name')
     
     # Choices for forward and prior models should be entered as arrays separated by commas
     # Currently there is only one option for a prior model, but this will be updated in the future.
-    parser.add_argument('forwards', nargs = '*', default = [0]) # 0 is for AT, 1 for bicubic
+    parser.add_argument('forwards', nargs = '*', default = [1]) # 0 is for AT, 1 for bicubic
     parser.add_argument('denoisers', nargs = '*', default = [1])
     
     # Read in the user-defined arguments.
@@ -207,20 +205,10 @@ if __name__ == '__main__':
     lrimg_file = os.path.join(base_path, 'images/LR images')
     resultsimg_file = os.path.join(base_path, 'images/results')
     
-    lrname = 'lrL='+str(args.SRval)+str(args.hrname)+'noise'+str(args.sigy)+'.png'
-
-    # Read in the high-resolution ground truth.
-    gt = cv2.imread(os.path.join(testimg_file,args.hrname),0)/255
     
     # Initialize synthetic low-resolution image.
     
-    if os.path.exists('LR Images/lrL='+str(args.SRval)+str(args.hrname)+'noise'+str(args.sigy)+'.png'):
-        lr = cv2.imread('LR Images/lrL='+str(args.SRval)+str(args.hrname)+'noise'+str(args.sigy)+'.png',0)/255
-    else:
-        lr = conv2d.Agen(gt,args.SRval)/(args.SRval*args.SRval)+np.random.normal(0,args.sigy,(gt.shape[0]//args.SRval,gt.shape[1]//args.SRval))
-    lr = np.float64(lr)
-    cv2.imwrite(os.path.join(lrimg_file, lrname), lr*255)
-    LR = cv2.imread(os.path.join(lrimg_file, lrname), 0)/255
+    LR = cv2.imread(os.path.join(lrimg_file, args.lrname),0)/255
 
     # Initialize other parameters.
     varn = args.sign**2
@@ -230,18 +218,15 @@ if __name__ == '__main__':
     numagents = len(args.denoisers) + len(args.forwards)
     
     # Run the MACE algorithm.
-    outW, PSNR, maceerr= mace(LR,numagents, c, args)
+    outW, maceerr= mace(LR,numagents, c, args)
     cv2.imwrite(os.path.join(resultsimg_file,str(args.iterstop)+'iters-DnCNNmaceout'+str(args.mu)+'.noise'+str(args.sign)+'.png'),outW.reshape(LR.shape[0]*args.SRval,LR.shape[0]*args.SRval)*255)
 
     # Load images for metric purposes
-    gt = cv2.imread(os.path.join(testimg_file,args.hrname),0)
     srx = cv2.imread(os.path.join(resultsimg_file,str(args.iterstop)+'iters-DnCNNmaceout'+str(args.mu)+'.noise'+str(args.sign)+'.png'),0)
     print("Path to the reconstruction is: ", resultsimg_file)
 
 
-    # Calculate PSNR for the final reconsruction and return our convergence metric.
-    ps = psnr.psnr(gt/255,srx/255)
-    print("PSNR for Our Reconstruction: ", ps)
+    # Return our convergence metric.
     print("MACE Error for Our Reconstruction: ", maceerr[-1])
     
     #Display our reconstruction
@@ -249,10 +234,3 @@ if __name__ == '__main__':
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     cv2.waitKey(1)
-
-
-
-
-
-    
-    
